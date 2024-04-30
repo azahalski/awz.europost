29eda5ee01d540b53ec2d4e75bdbb80404de34af
Andrew Zahalski <info@zahalski.dev>
2024-04-30 07:29:20 +0300

    gulp checkpoint

---
+++ update/1.1.3/lang/ru/options.php
@@ -0,0 +1,24 @@
+<?
+$MESS["AWZ_EUROPOST_OPT_TITLE"] = "Настройки модуля AWZ: Европочта";
+$MESS["AWZ_EUROPOST_OPT_SECT1"] = "Общие настройки";
+$MESS["AWZ_EUROPOST_OPT_SECT2"] = "Настройки #PROFILE_NAME#";
+$MESS["AWZ_EUROPOST_OPT_SECT3"] = "Права доступа";
+$MESS["AWZ_EUROPOST_OPT_MAP_ADRESS"] = "Включить строку поиска адреса на Яндекс Карте (обязателен ключ API)";
+$MESS["AWZ_EUROPOST_OPT_MAP_ADRESS_DESC"] = "Кабинет разработчика на Яндексе";
+$MESS["AWZ_EUROPOST_OPT_MAP_ADRESS_KEY1"] = "Ключ JavaScript API";
+$MESS["AWZ_EUROPOST_OPT_MAP_ADRESS_KEY2"] = "Ключ API Геосаджеста";
+$MESS["AWZ_EUROPOST_OPT_L_GROUP1"] = "Загрузка и обновление списка ПВЗ";
+$MESS["AWZ_EUROPOST_OPT_L_GROUP1_DESC"] = "Отметьте опции и сохраните настройки! <br> Список ПВЗ обновляется автоматически на агенте 1 раз в неделю.";
+$MESS["AWZ_EUROPOST_OPT_L_DELPVZ"] = "Удалить ПВЗ из базы";
+$MESS["AWZ_EUROPOST_OPT_L_UPPVZ"] = "Обновить ПВЗ в базе";
+$MESS["AWZ_EUROPOST_OPT_L_PROFILE_PROP"] = "Привязка свойств";
+$MESS["AWZ_EUROPOST_OPT_L_PROPPVZ"] = "Код свойства заказа в которое писать ид пвз";
+$MESS["AWZ_EUROPOST_OPT_L_PROPPVZ_ADR"] = "Код свойства заказа в которое писать адрес пвз";
+$MESS["AWZ_EUROPOST_OPT_L_PROPPVZ_ADR_TMPL"] = "Шаблон адреса ПВЗ";
+$MESS["AWZ_EUROPOST_OPT_L_PROPPVZ_ADR_TMPL_DESC"] = "#ID# - ид ПВЗ, <br>#NAME# - название ПВЗ";
+$MESS["AWZ_EUROPOST_OPT_L_BTN_SAVE"] = "Сохранить";
+$MESS["AWZ_EUROPOST_OPT_MESS1"] = "ПВЗ удалены";
+$MESS["AWZ_EUROPOST_OPT_MESS2"] = "ПВЗ загружены";
+$MESS["AWZ_EUROPOST_OPT_L_SOOT"] = "Соответствие местоположений";
+$MESS["AWZ_EUROPOST_OPT_L_SOOT_LBL1"] = "ЕвроПочта";
+$MESS["AWZ_EUROPOST_OPT_L_SOOT_LBL2"] = "Битрикс";
