1b8133a57c455b6a7b453c1f170988519b7f8ce6
Andrew Zahalski <info@zahalski.dev>
2024-12-08 17:14:35 +0300

    gulp checkpoint

---
+++ update/1.2.3/install/version.php
@@ -0,0 +1,5 @@
+<?
+$arModuleVersion = array(
+    "VERSION" => "1.2.3",
+    "VERSION_DATE" => "2024-12-08 17:13:00"
+);
---
+++ update/1.2.3/lang/ru/lib/access/custom/componentconfig.php
@@ -0,0 +1,5 @@
+<?php
+/*awz.gen start - !!!nodelete*/
+$MESS['AWZ_CONFIG_PERMISSION_SECTION_GENS'] = 'Генераторы';
+$MESS['AWZ_CONFIG_PERMISSION_SECTION_ADMIN'] = 'Административная панель';
+/*awz.gen end - !!!nodelete*/
